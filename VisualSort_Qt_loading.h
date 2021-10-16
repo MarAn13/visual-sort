2566ac3984155067deb097ebcfacf04f3cbf8bff
marAn <maremianin@gmail.com>
2021-10-16 23:38:09 +0300

    added several const

--- VisualSort_Qt_loading.h
+++ VisualSort_Qt_loading.h
@@ -17,7 +17,7 @@ private:
 	const std::string qrc_prefix = ":/visual_sort/";
 	const std::string path_to_folder = "media/sort_anim/";
 	const int file_paths_num = 23;
-	std::string file_paths[23] = { "sort_state_1.png", "sort_state_2_0.png", "sort_state_2_1.png", "sort_state_3_0.png", "sort_state_3_1.png", "sort_state_4_0.png",
+	const std::string file_paths[23] = { "sort_state_1.png", "sort_state_2_0.png", "sort_state_2_1.png", "sort_state_3_0.png", "sort_state_3_1.png", "sort_state_4_0.png",
 	"sort_state_4_1.png", "sort_state_5_0.png", "sort_state_5_1.png", "sort_state_6_0.png", "sort_state_6_1.png", "sort_state_7_0.png", "sort_state_7_1.png",
 		"sort_state_8_0.png", "sort_state_8_1.png", "sort_state_9_0.png", "sort_state_9_1.png", "sort_state_10_0.png", "sort_state_10_1.png", "sort_state_11_0.png",
 		"sort_state_11_1.png", "sort_state_12_0.png", "sort_state_12_1.png" };
